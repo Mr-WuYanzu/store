38c70984ede913824fb860a4b1511ff3325ed3d9
jennifer <1793880774@qq.com>
2019-05-24 00:32:34 +0000

    用户注册登录

--- routes/web.php
+++ routes/web.php
@@ -1,3 +1,12 @@
 <?php
 
 Route::get('/','home\HomeController@index');
+
+Route::get('/register','User\UserController@register');//注册
+Route::post('/reg_do','User\UserController@reg_do');//注册执行
+Route::get('/login','User\UserController@login');//登录
+Route::post('/login_do','User\UserController@login_do');//登录执行
+Route::get('/forget','User\UserController@forget');//忘记密码
+Route::post('/forget_do','User\UserController@forget_do');//执行找回密码
+Route::get('/new_password','User\UserController@new_password');//设置新密码视图
+Route::post('/set_new_password','User\UserController@set_new_password');//执行设置新密码
