569430730052befd61031c6a6d7598e966b0c728
wuyanzu <1311470691@qq.com>
2019-05-24 19:02:40 +0800

    收藏页面

--- resources/views/collect/wishlist.blade.php
+++ resources/views/collect/wishlist.blade.php
@@ -4,46 +4,7 @@
 
 @section('content')
 
-    <!-- side nav right-->
-    <div class="side-nav-panel-right">
-        <ul id="slide-out-right" class="side-nav side-nav-panel collapsible">
-            <li class="profil">
-                <img src="img/profile.jpg" alt="">
-                <h2>John Doe</h2>
-            </li>
-            <li><a href="setting.html"><i class="fa fa-cog"></i>Settings</a></li>
-            <li><a href="about-us.html"><i class="fa fa-user"></i>About Us</a></li>
-            <li><a href="contact.html"><i class="fa fa-envelope-o"></i>Contact Us</a></li>
-            <li><a href="login.html"><i class="fa fa-sign-in"></i>Login</a></li>
-            <li><a href="register.html"><i class="fa fa-user-plus"></i>Register</a></li>
-        </ul>
-    </div>
-    <!-- end side nav right-->
 
-    <!-- navbar bottom -->
-    <div class="navbar-bottom">
-        <div class="row">
-            <div class="col s2">
-                <a href="index.html"><i class="fa fa-home"></i></a>
-            </div>
-            <div class="col s2">
-                <a href="wishlist.html"><i class="fa fa-heart"></i></a>
-            </div>
-            <div class="col s4">
-                <div class="bar-center">
-                    <a href="#animatedModal" id="cart-menu"><i class="fa fa-shopping-basket"></i></a>
-                    <span>2</span>
-                </div>
-            </div>
-            <div class="col s2">
-                <a href="contact.html"><i class="fa fa-envelope-o"></i></a>
-            </div>
-            <div class="col s2">
-                <a href="#animatedModal2" id="nav-menu"><i class="fa fa-bars"></i></a>
-            </div>
-        </div>
-    </div>
-    <!-- end navbar bottom -->
 
     <!-- wishlist -->
     <div class="wishlist section">
