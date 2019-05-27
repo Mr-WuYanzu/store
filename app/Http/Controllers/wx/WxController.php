8bc9c049df0a6f4f3c361e8eb153160a5e5e0b07
litonngxiao <1073074517@qq.com>
2019-05-27 16:51:12 +0800

    debug

--- app/Http/Controllers/wx/WxController.php
+++ app/Http/Controllers/wx/WxController.php
@@ -10,14 +10,14 @@ class WxController extends Controller
     //
     public function wx(){
 //        $a= urlEncode("http://store.zhbcto.com/wx");
-//        $url="https://open.weixin.qq.com/connect/oauth2/authorize?appid=wx71cba979e28407f8&redirect_uri=".$a."&response_type=code&scope=snsapi_base &state=STATE#wechat_redirect";
+//        $url="https://open.weixin.qq.com/connect/oauth2/authorize?appid=wx71cba979e28407f8&redirect_uri=".$a."&response_type=code&scope=snsapi_base&state=STATE&connect_redirect=1#wechat_redirect";
 //        echo $url;die;
 
         $code=$_GET['code'];
         $access_token_url="https://api.weixin.qq.com/sns/oauth2/access_token?appid=wx71cba979e28407f8&secret=764edbb06c265cfce2c7dca5f5128e25&code=".$code."&grant_type=authorization_code";
         $access_token=file_get_contents($access_token_url);
         $arr=json_decode($access_token);
-        dd($arr);
+
         $token=$arr->access_token;
         $openid=$arr->openid;
 
@@ -52,6 +52,5 @@ class WxController extends Controller
             echo "绑定成功";
             session(['user'=>['user_name'=>$user_info['user_name'],'user_id'=>$user_info['user_id']]]);
         }
-
     }
 }
